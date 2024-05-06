15396ff8c78062476a5179b5280ae33aa8b04c26
simonayo <ayosimon.dev@gmail.com>
2024-05-06 19:08:29 +0100

    update

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
+  isSticky = false;
 
+  @HostListener('window:scroll', [])
+  checkScroll() {
+    const scrollPosition = window.pageYOffset;
+    this.isSticky = scrollPosition >= 100; 
+  }
 }
